fdaabe3208b6ae991cdcad1f91224d27feac5d66
sm_lnu <mykhailo.stefanyshyn@lnu.edu.ua>
2023-12-09 13:43:42 +0200

    Start point of developing Test Server

---
+++ Test Server/LoginningForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Test_Server
+{
+    public partial class LoginningForm : Form
+    {
+        private string Login;
+        private string Password;
+
+        public LoginningForm()
+        {
+            InitializeComponent();
+            textBox2.UseSystemPasswordChar = true;
+            CheckEnableLoginButton();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Login = textBox1.Text;
+            CheckEnableLoginButton();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            Password = textBox2.Text;
+            CheckEnableLoginButton();
+        }
+
+        private void CheckEnableLoginButton()
+        {
+            button1.Enabled = !string.IsNullOrEmpty(Login) && !string.IsNullOrEmpty(Password);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
